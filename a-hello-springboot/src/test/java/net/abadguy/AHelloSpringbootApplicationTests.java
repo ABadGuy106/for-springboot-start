c87613d4c5b3e6d05dd2cbbea9960c5afc7bcbeb
ABadGuy <abadguy@abadguy.com>
2018-08-14 14:44:47 +0800

    springboot日志使用方式

--- a-hello-springboot/src/test/java/net/abadguy/AHelloSpringbootApplicationTests.java
+++ a-hello-springboot/src/test/java/net/abadguy/AHelloSpringbootApplicationTests.java
@@ -2,6 +2,8 @@ package net.abadguy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AHelloSpringbootApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+		Logger logger = LoggerFactory.getLogger(getClass());
+
+		//日志的级别
+		//由低到高
+		logger.trace("这是trace日志————————————");
+		logger.debug("这是debug信息#######################");
+		logger.info("这是info日志@@@@@@@@@@@@@@@@@@@@@@@@@");
+		logger.warn("这是warn日志+++++++++++++++++++++++++");
+		logger.error("这是error日志***********************");
+
 	}
 
 }
